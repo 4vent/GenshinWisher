188d6fcfd6b64ef7e0049496bf09a60bc01ffaac
Avent <44436695+abeckent@users.noreply.github.com>
2023-01-05 03:03:01 +0900

    asdfas

--- GachaSimurator/wish_full.py
+++ GachaSimurator/wish_full.py
@@ -9,21 +9,22 @@ from wish import EventWish
 # COST = 1.57  # 3,880 /  6,100 yen
 COST = 1.49  # 8,080 / 12,000 yen
 
-if __name__ == "__main__":
+
+def main(ljust_width=18):
     from ljust_eastasianwidth import ljust2
 
     pu5 = [c.Raiden_Shogun]
     pu4 = [c.Kujou_Sara, c.Sayu, c.Rosaria]
     wish = EventWish(pu5, pu4)  # type: ignore
-    fee = 0
+    gems = 0
 
     hit_chars: Dict[c.Charactor, int] = {}
     while True:
         pu4count = 0
         pu5count = 0
-        fee += 1600 * COST
-        print(f"¥{fee:,.0f}".ljust(8) + ' | ', end='')
-        for _ in range(10):
+        gems += 1600
+        print(f"¥{gems * COST:,.0f} / {gems:,} gems")
+        for i in range(10):
             result = wish.wish()
             if result.rarity == 5:
                 if result in pu5:
@@ -37,7 +38,9 @@ if __name__ == "__main__":
                     print('\033[38;5;045m', end='')
                 else:
                     print('\033[38;5;053m', end='')
-            print(ljust2(result.name, 16), end='')
+            print(ljust2(result.name, ljust_width), end=' ')
+            if i == 4:
+                print('')
             print('\033[0m', end='')
 
             if isinstance(result, c.Charactor):
@@ -45,10 +48,10 @@ if __name__ == "__main__":
                     hit_chars[result] = -1
                 hit_chars[result] += 1
         
-        for _ in range(pu4count):
-            print('\033[38;5;045m*4PU\033[0m ', end='')
-        for _ in range(pu5count):
-            print('\033[38;5;220m*5PU!\033[0m ', end='')
+        # for _ in range(pu4count):
+        #     print('\033[38;5;045m*4PU\033[0m ', end='')
+        # for _ in range(pu5count):
+        #     print('\033[38;5;220m*5PU!\033[0m ', end='')
         
         print('')
         for k, v in hit_chars.items():
@@ -65,8 +68,13 @@ if __name__ == "__main__":
                 else:
                     print('\033[38;5;053m', end='')
 
-            print(ljust2(k.name + ':' + ('無' if v == 0 else str(v) if v < 6 else f'完({v})') + '凸', 20), end=' ')
+            print(ljust2(k.name + ':' + ('無' if v == 0 else str(v) if v < 6 else f'完({v})') + '凸', ljust_width), end='\n')
             print('\033[0m', end='')
         
         if not input('\n') == '':
             break
+
+
+
+if __name__ == "__main__":
+    main(10)
